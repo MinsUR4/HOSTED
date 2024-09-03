3fb6a3036a45c9c185735eae08971424ba706300
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2024-09-03 16:33:29 -0400

    Update and rename thes.ebg.eg.op.mds40.csshosted.html5.js to xml.encasing.ebg.forever.esc:accesscode=dgdue9igsdfjec.ur40upload.thes.ebg.eg.op.mds40.csshosted.html5.xml

--- thes.ebg.eg.op.mds40.csshosted.html5.js
+++
@@ -1,4 +0,0 @@
-const htmlContent = `<html><head></head><body style="margin: 0px; height: 100vh;"><iframe src="https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/MinsUR4/HOSTED@main/xml.encasing.ebg.forever.esc:accesscode=dgdue9igsdfjec.ur40upload.xml&amp;container=ig" style="border: none; width: 100%; height: 100%; margin: 0px;"></iframe></body></html>`;
-const div = document.createElement('div');
-div.innerHTML = htmlContent;
-document.body.appendChild(div);
