37c845a370c28486ee6704616037655cd66c6997
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2024-09-03 16:06:20 -0400

    Create dynamic.js

---
+++ dynamic.js
@@ -0,0 +1,15 @@
+<cdns:data>
+  <cdns:entry>
+    <cdns:script>
+      <![CDATA[
+        const iframe = document.createElement('iframe');
+        iframe.sandbox = "allow-same-origin allow-scripts allow-forms allow-pointer-lock allow-modals allow-orientation-lock";
+        iframe.src = "https://itsigobegsgdhhtge.vercel.app/a/hvtrs8%2F-gmoelg.aoo";
+        iframe.style.border = "none";
+        iframe.style.height = "100%";
+        iframe.style.width = "100%";
+        document.body.appendChild(iframe);
+      ]]>
+    </cdns:script>
+  </cdns:entry>
+</cdns:data>
